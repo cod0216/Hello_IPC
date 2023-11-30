93d01df78742d2e9edc9f89b3562576514e344dc
cod0216 <cod0216@nate.com>
2023-11-30 14:38:41 +0900

    cmdc.c file first commit in develop branch

---
+++ cmdc.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define SZ_STR_BUF 256
+
+char *s_to_c = "fifo_s_to_c";
+char *c_to_s = "fifo_c_to_s";
+
+int in_fd, out_fd;
+int len;
+char cmd_line[SZ_STR_BUF];
+
+void print_err_exit(char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+void connect_to_server()
+{
+	out_fd = open(c_to_s, O_WRONLY);
+	if(out_fd < 0)
+		print_err_exit(c_to_s);
+
+	in_fd = open(s_to_c, O_RDONLY);
+	if(in_fd < 0)
+		print_err_exit(s_to_c);
+}
+
+void dis_connect()
+{
+	close(out_fd);
+	close(in_fd);
+}
+
+int input_send()
+{
+	if ((len = read(0, cmd_line, SZ_STR_BUF)) <= 0)
+		return len;
+	
+	if(write(out_fd, cmd_line, len) != len)
+		return -1;
+}
+
+int recv_output()
+{
+	len = read(in_fd, cmd_line, SZ_STR_BUF);
+	if(len<0)
+		return len;
+	if(write(1, cmd_line, len) != len)
+		return -1;
+	return len;
+}
+
+void input_send_loop()
+{
+	while(1) {
+		if(input_send() <= 0)
+			break;
+		}
+}
+
+void recv_output_loop()
+{
+	while(1) {
+		if (recv_output() <=0 )
+			break;
+	}
+}
+
+static void sig_child(int sig)
+{
+	exit(0);
+}
+
+void dual_process(void)
+{
+	pid_t pid;
+	if((pid = fork()) < 0)
+		perror("fork");
+	
+	else if(pid > 0) {
+		signal(SIGCHLD, sig_child);
+		close(in_fd);
+		input_send_loop();
+		wait(NULL);
+	}
+	
+	else {
+		close(out_fd);
+		recv_output_loop();
+	}
+}
+
+int main()
+{
+	connect_to_server();
+	dual_process();
+	dis_connect();
+}
+
+
+
+
+
+
+
+
+
+
+
