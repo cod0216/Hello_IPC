0ca83ad9a9c9dfc485b84cbd3ea7d884f089d206
cod0216 <cod0216@nate.com>
2023-12-01 12:09:01 +0900

    fixed cmdc.c file

--- cmdc.c
+++ cmdc.c
@@ -3,9 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
 
 #define SZ_STR_BUF 256
 
@@ -22,90 +19,62 @@ void print_err_exit(char *msg)
 	exit(1);
 }
 
-void connect_to_server()
-{
-	out_fd = open(c_to_s, O_WRONLY);
-	if(out_fd < 0)
-		print_err_exit(c_to_s);
-
-	in_fd = open(s_to_c, O_RDONLY);
-	if(in_fd < 0)
-		print_err_exit(s_to_c);
-}
-
-void dis_connect()
+void dis_connect() 
 {
 	close(out_fd);
 	close(in_fd);
 }
 
+void connect_to_server()
+{
+	out_fd = open(c_to_s, O_WRONLY);
+	if(out_fd < 0)
+		print_err_exit(c_to_s);
+		in_fd = open(s_to_c, O_RDONLY);
+		if(in_fd < 0)
+			print_err_exit(s_to_c);
+}
+
 int input_send()
 {
-	if ((len = read(0, cmd_line, SZ_STR_BUF)) <= 0)
+	if((len = read(0, cmd_line, SZ_STR_BUF)) <= 0)
 		return len;
-	
+		
 	if(write(out_fd, cmd_line, len) != len)
 		return -1;
-
 	return len;
 }
 
 int recv_output()
 {
 	len = read(in_fd, cmd_line, SZ_STR_BUF);
-	if(len<0)
+	if(len<0) 
 		return len;
+	
 	if(write(1, cmd_line, len) != len)
 		return -1;
+	
 	return len;
 }
 
-void input_send_loop()
+void single_process()
 {
 	while(1) {
 		if(input_send() <= 0)
 			break;
-		}
-}
-
-void recv_output_loop()
-{
-	while(1) {
-		if (recv_output() <=0 )
+		if (recv_output() <= 0)
 			break;
 	}
 }
 
-static void sig_child(int sig)
-{
-	exit(0);
-}
-
-void dual_process(void)
-{
-	pid_t pid;
-	if((pid = fork()) < 0)
-		perror("fork");
-	
-	else if(pid > 0) {
-		signal(SIGCHLD, sig_child);
-		close(in_fd);
-		input_send_loop();
-		wait(NULL);
-	}
-	
-	else {
-		close(out_fd);
-		recv_output_loop();
-	}
-}
 
 int main()
 {
 	connect_to_server();
-	dual_process();
+	single_process();
 	dis_connect();
 }
+
 
 
 
